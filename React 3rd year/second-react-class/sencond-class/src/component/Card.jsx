85cd851dff627b87599b6343c15bdd8001017c1d
Ankit <ankit0525252@gmail.com>
2025-10-31 14:55:48 +0530

    created card component

---
+++ React 3rd year/second-react-class/sencond-class/src/component/Card.jsx
@@ -0,0 +1,11 @@
+import './Card.css'
+
+function Card(){
+  return(
+    <div id="card">
+      <img src="https://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg" alt="image.jpeg" height={150} width={150}/>
+    </div>
+  )
+}
+
+export default Card;
